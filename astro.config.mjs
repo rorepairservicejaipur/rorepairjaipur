29117f3680907099e2c65967ebb73940e0d30089
rorepairservicejaipur <abubakarcompanion@gmail.com>
2026-08-10 14:55:44 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://humbornjo.github.io",
+  site: "https://rorepairservicejaipur.github.io",
   base: "/",
   prefetch: {
     prefetchAll: true,
